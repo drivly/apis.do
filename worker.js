9b000a018aecb2962627bc39822d71ead03d50f3
Nathan Clevenger <nateclev@gmail.com>
2022-09-05 06:15:40 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,25 +1,30 @@
+import { Router } from 'itty-router'
+import { json, withParams } from 'itty-router-extras'
+
+const router = Router()
+
+const api = {
+  icon: '🚀',
+  name: 'apis.do',
+  description: 'Cloudflare API',
+  url: 'https://apis.do/api',
+  typeOf: 'https://apis.do/integrations',
+  endpoints: {
+    primitives: 'https://apis.do/primitives',
+    pipes: 'https://apis.do/pipes',
+    integrations: 'https://apis.do/integrations',
+  },
+  site: 'https://apis.do',
+  repo: 'https://github.com/drivly/api.cf',
+}
+
+router.all('*', async (req, env, ctx) => {
+  req.user = await env.CTX.fetch(req).then(res => res.json())
+})
+
+router.get('/api')
+
+
 export default {
-  fetch: async (req, env) => {
-    const { user, redirect, body } = await env.CTX.fetch(req).then(res => res.json())
-    if (redirect) return Response.redirect(redirect)
-    
-    const { origin, pathname, search } = new URL(req.url)
-    
-    return new Response(JSON.stringify({
-      api: {
-        icon: '🚀',
-        name: 'apis.do',
-        description: 'Composable APIs from Drivly Open',
-        url: 'https://apis.do',
-        api: 'https://apis.do/api',
-        endpoints: {
-          parse: origin + '/parse?prop=value',
-          generate: origin + '/:url',
-        },
-        type: 'https://apis.do',
-        repo: 'https://github.com/drivly/apis.do',
-      },
-      user,
-    }, null, 2), { headers: { 'content-type': 'application/json; charset=utf8' }}) 
-  }
+  fetch: router.handle 
 }
