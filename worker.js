53347e78d4749ef0e210f2850e03b1bd1497d103
Nathan Clevenger <nateclev@gmail.com>
2022-09-23 05:18:42 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,31 +1,23 @@
-import { Router } from 'itty-router'
-import { json, withParams } from 'itty-router-extras'
-
-const router = Router()
-
-const api = {
+export const api = {
   icon: '🚀',
   name: 'apis.do',
   description: 'Cloudflare API',
   url: 'https://apis.do/api',
-  typeOf: 'https://apis.do/integrations',
+  type: 'https://apis.do/api',
   endpoints: {
-    primitives: 'https://apis.do/primitives',
-    pipes: 'https://apis.do/pipes',
-    integrations: 'https://apis.do/integrations',
+    types: 'https://apis.do/api',
+    pipes: 'https://apis.do/:type',
   },
   site: 'https://apis.do',
+  login: 'https://apis.do/login',
+  signup: 'https://apis.do/signup',
   repo: 'https://github.com/drivly/apis.do',
 }
 
-router.all('*', async (req, env, ctx) => {
-  req.user = await env.CTX.fetch(req).then(res => res.json()).user
-})
-
-router.get('/api', ({user}) => json({ api, user }))
-
-router.all('*', ({user}) => json({ api, user }))
-
 export default {
-  fetch: router.handle 
+  fetch: async (req, env) => {
+    const { user, origin, requestId, method, body, time, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
+    return new Response(JSON.stringify({ api, requestId, pathSegments, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+  }
 }
+
