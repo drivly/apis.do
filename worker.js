f93c6a7f7141be50a11865ba9597a457bd7edd34
Nathan Clevenger <nateclev@gmail.com>
2022-09-27 18:41:22 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -16,12 +16,13 @@ export const api = {
 }
 
 const apis = {
-  core: {
+  apis: {
     'APIs': 'https://apis.do/api',
     'API Context': 'https://ctx.do/api',
     'API Directory': 'https://apis.directory/api',
     'API Management': 'https://api.mgmt.do/api',
     'API Testing': 'https://api.qa/api',
+    'GraphQL': 'https://graphql.do/api',
     'Proxy': 'https://proxies.do/api',
     'Webhooks': 'https://webhooks.do/api',
   },
@@ -30,9 +31,27 @@ const apis = {
     'Triggers': 'https://triggers.do/api',
     'Searches': 'https://searches.do/api',
     'Actions': 'https://actions.do/api',
+    'Entity': 'https://entity.do/api',
+    'Resource': 'https://resource.do/api',
+    'CRUD': 'https://crud.do/api',
+  },
+  glyph: {
+    '入 Function': 'https://入.io/api',
+    '凵 Schema': 'https://凵.io/api',
+    '口 Resource': 'https://口.io/api',
+    '回 Entity': 'https://回.io/api',
+    '巛 Function': 'https://巛.io/api',
+    'ılıl Analytics': 'https://ılıl.com/api',
+    '彡 Database': 'https://彡.io/api',
+    '亘 Page': 'https://亘.io/api',
+    '目 List': 'https://目.io/api',
+    '田 Collection': 'https://田.io/api',
+    '卌 Counter': 'https://卌.io/api',
   },
   saas: {
     'Analytics': 'https://analytics.do/api',
+    'Customers': 'https://customers.do/api',
+    'Dashboard': 'https://dash.do/api',
     'Funnels': 'https://funnels.do/api',
     'Subscriptions': 'https://subscriptions.do/api',
     'Users': 'https://users.do/api',
@@ -47,11 +66,19 @@ const apis = {
     'Lodash': 'https://lodash.do/api',
     'Pluck': 'https://pluck.do/api',
   },
+  tools: {
+    'Count': 'https://count.do/api',
+    'Counters': 'https://counters.do/api',
+    'Debug': 'https://debug.do/api',
+  },
   utilities: {
+    'Decode': 'https://decode.do/api',
+    'Flatten': 'https://flatten.do/api',
     'camelCaseKeys': 'https://camel.case.do/api',
     'kebab-case-keys': 'https://kebab.case.do/api',
     'snake_case_keys': 'https://snake.case.do/api',
     'Title Case Keys': 'https://title.case.do/api',
+    'Filter': 'https://filter.do/api',
   },
   formats: {
     'CSV Conversions': 'https://csv.do/api',
@@ -60,6 +87,16 @@ const apis = {
     'TypeScript Conversions': 'https://markdown.do/api',
     'YAML Conversions': 'https://yaml.do/api',
     ;
+  },
+  convert: {
+    'Convert': 'https://convert.do/api',
+  },
+  crypto: {
+    'Encrypt': 'https://encrypt.do/api',
+    'Decrypt': 'https://decrypt.do/api',
+    'Hashes': 'https://hashes.do/api',
+    'HMAC': 'https://hmac.do/api',
+    'Vault': 'https://vault.do/api',
   },
   state: {
     'Finite State Machines': 'https://state.do/api',
@@ -74,18 +111,55 @@ const apis = {
   data: {
     'Database': 'https://database.do/api',
     'Bucket': 'https://database.do/api',
+    'Examples': 'https://examples.do/api',
   },
   cicd: {
     'Builds': 'https://builds.do/api',
+    'Deploys': 'https://deploys.do/api',
+    'Environment': 'https://env.do/api',
+  },
+  http: {
+    'Proxy': 'https://prxy.do/api',
+  },
+  product: {
+    'Features': 'https://features.do/api',
+    'Feature Flags': 'https://feature.flags.do/api',
+  },
+  events: {
+    'Events': 'https://events.do/api',
+    'Triggers': 'https://triggers.do/api',
+    'Alarms': 'https://alarms.do/api',
+    'Discord': 'https://discord.do/api',
+    'Email': 'https://emails.do/api',
+    'Text': 'https://texts.do/api',
+    'Slack': 'https://slack.do/api',
+    'Timer': 'https://timer.do/api',
+    'Webhooks': 'https://webhooks.do/api',
   },
   web: {
     'Caches': 'https://caches.do/api',
+    'Cookies': 'https://cookies.do/api',
+    'CORS': 'https://cors.do/api',
+    'Downloads': 'https://downloads.do/api',
+    'Embed': 'https://embed.do/api',
     'Fetch': 'https://fetch.do/api',
+    'Fetcher': 'https://fetcher.do/api',
+    'Files': 'https://files.do/api',
+    'Flows': 'https://flows.do/api',
     'Scraper': 'https://scraper.do/api',
+    'Rewrites': 'https://rewrites.do/api',
+    'Redirects': 'https://redirects.do/api',
+  },
+  domains: {
+    'CNAME Proxy': 'https://cname.do/api',
+    'DDNS Service': 'https://ddns.do/api',
+    'Hostnames': 'https://hostnames.do/api',
+    'Hostname API': 'https://hostname.do/api',
   },
   durableObjects: {
     'Alarms': 'https://alarms.do/api',
     'Backup': 'https://backup.do/api',
+    'Copy': 'https://copy.do/api',
     'Restore': 'https://restore.do/api',
   },
   analytics: {
@@ -98,16 +172,32 @@ const apis = {
     'Text': 'https://texts.do/api',
     'Slack': 'https://slack.do/api',
   },
+  functions: {
+    'Functions': 'https://functions.do/api',
+    'Dynamic Function': 'https://function.do/api',
+  },
   schema: {
     'Schema Generation': 'https://schema.do/api',
     'GraphQL Schema': 'https://gql.do/api',
   },
   personas: {
     'Builders': 'https://builders.do/api',
+    'Employees': 'https://employees.do/api',
+    'Engineers': 'https://engineers.do/api',
+    'Humans': 'https://humans.do/api',
+  },
+  databases: {
+    'Database': 'https://database.do/api',
+    'GraphDL': 'https://graphdl.org/api',
+    'Graph Database': 'https://graph.org/api',
   },
   pipes: {
-    'Chains': 'https://chains.do/api',
     'Pipes': 'https://pipes.do/api',
+    'Chains': 'https://chains.do/api',
+    'Commands': 'https://commands.do/api',
+    'Compose': 'https://compose.do/api',
+    'Console': 'https://console.do/api',
+    'Grep': 'https://grep.do/api',
   },
   ai: {
     'Artificial Intelligences': 'https://ais.do/api',
@@ -118,6 +208,9 @@ const apis = {
     'Abstract Syntax Tree Generation': 'https://syntax.do/api',
     'Algorithm Library': 'https://algorithms.do/api',
     'ESBuild Transpilation': 'https://esbuild.do/api',
+    'ES6 ': 'https://es6.do/api',
+    'Eval ': 'https://eval.do/api',
+    'Gist ': 'https://gist.do/api',
   },
   testing: {
     'API Testing': 'https://api.qa/api',
@@ -127,10 +220,13 @@ const apis = {
     'Status Page': 'https://status.page.as/api',
   },
   cloudflare: {
+    'Cloudflare': 'https://cloudflare.do/api',
     'API': 'https://api.cf/api',
+    'Colo': 'https://colo.do/api',
     'D1': 'https://d1.cf/api',
     'Durable Objects': 'https://do.cf/api',
     'DO Alarms': 'https://alarms.cf/api',
+    'Environment': 'https://env.do/api',
     'KV': 'https://kv.cf/api',
     'Service Bindings': 'https://service.do/api',
     'Workers': 'https://workers.cf/api',
@@ -140,26 +236,56 @@ const apis = {
     'Airtable Base API': 'https://base.do/api',
     'Zapier Zap Generation': 'https://zap.do/api',
   },
+  design: {
+    'icons': 'https://icons.do/api',
+  },
   marketing: {
     'API Landing Pages': 'https://api.page/api',
     'Blogs': 'https://blogs.do/api',
     'Chat Boxes': 'https://chat.boxes.do/api',
+    'Content': 'https://content.do/api',
+    'Drip Sequences': 'https://drip.do/api',
+    'Glyph': 'https://glyph.do/api',
+    'Product Hunts': 'https://product.hunts.do/api',
   },
   commerce: {
     'ACH Transfers': 'https://ach.do/api',
     'Bill of Sale': 'https://bos.do/api',
     'Checkout': 'https://checkout.do/api',
+    'eContract': 'https://econtract.do/api',
     'Subscriptions': 'https://subscriptions.do/api',
   },
   workflows: {
     'Workflows': 'https://workflows.do/api',
+    'Concierge': 'https://concierge.do/api',
+  },
+  apps: {
+    'CRMs': 'https://crms.do/api',
+    'Concierge': 'https://concierge.do/api',
   },
   automotive: {
     'Automotive Data': 'https://auto.dev/api',
     'Auto Commerce APIs': 'https://driv.ly/api',
+    'Dealer Service': 'https://dealers.do/api',
+    'Dealer API': 'https://dealer.to/api',
+    'Electric Vehicles API': 'https://evs.to/api',
+  },
+  startup: {
+    'Experiment': 'https://experiment.do/api',
+    'Goals': 'https://goals.do/api',
+    'Hiring': 'https://hiring.do/api',
+    'Ideate': 'https://ideate.do/api',
+  },
+  tbd: {
+    'directory.do': 'https://directory.do/api',
+    'encrypted.do': 'https://encrypted.do/api',
+  },
+  fun: {
+    '🚀 Emojis': 'https://builders.do/api',
   },
   jobs: {
     'Job Application': 'https://career.do/api',
+    'Project Gigs': 'https://gigs.do/api',
   },
 }
 
