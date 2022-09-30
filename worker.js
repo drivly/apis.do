63d7ab58252934d926b6f0bdd71d2689316ad948
Nathan Clevenger <nateclev@gmail.com>
2022-09-29 19:50:26 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,29 +1,29 @@
-import { getAPI } from './index' 
+// import { getAPI } from './index' 
 
 // import apis from './apis'
 
-// export const api = {
-//   icon: '🚀',
-//   name: 'apis.do',
-//   description: 'Hypermedia-driven API Directory',
-//   url: 'https://apis.do/api',
-//   type: 'https://apis.do/core',
-//   endpoints: {
-//     listAllAPIs: 'https://apis.do/apis',
-//     listCategories: 'https://apis.do/categories',
-//     getCategory: 'https://apis.do/:type',
-//     search: 'https://apis.do/search/:term',
-//   },
-//   site: 'https://apis.do',
-//   login: 'https://apis.do/login',
-//   signup: 'https://apis.do/signup',
-//   subscribe: 'https://apis.do/subscribe',
-//   repo: 'https://github.com/drivly/apis.do',
-// }
+export const api = {
+  icon: '🚀',
+  name: 'apis.do',
+  description: 'Hypermedia-driven API Directory',
+  url: 'https://apis.do/api',
+  type: 'https://apis.do/core',
+  endpoints: {
+    listAllAPIs: 'https://apis.do/apis',
+    listCategories: 'https://apis.do/categories',
+    getCategory: 'https://apis.do/:type',
+    search: 'https://apis.do/search/:term',
+  },
+  site: 'https://apis.do',
+  login: 'https://apis.do/login',
+  signup: 'https://apis.do/signup',
+  subscribe: 'https://apis.do/subscribe',
+  repo: 'https://github.com/drivly/apis.do',
+}
 
 export default {
   fetch: async (req, env) => {
-    const api = getAPI(req)
+//     const api = getAPI(req)
     const { user, origin, requestId, method, body, time, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
     const [ category ] = pathSegments
 //     const apis = categories[category] ?? categories
