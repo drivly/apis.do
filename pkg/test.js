a977ed3b7dd74b34ac19ab42eb36580aed9062cc
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-10-02 16:40:19 -0500

    Added test

--- pkg/test.js
+++ pkg/test.js
@@ -1,4 +1,7 @@
 import test from 'ava'
-import { apis } from './dist/index.js'
+import { apis } from './index.js'
 
-test('icon', t => t.is(apis['apis.do'].icon, '🚀'))
+test('icon', t => t.is(apis['apis.do'].icon, '🚀')) 
+
+
+test('workers', t => t.is(!!apis['worker.do'], true)) 
