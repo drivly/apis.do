268ddbb705d236c73ea65a7d3bb23d9f9a321756
Nathan Clevenger <nateclev@gmail.com>
2022-10-28 04:16:07 -0500

    Update markdown.js

--- markdown.js
+++ markdown.js
@@ -1,5 +1,12 @@
 export default {
   fetch: async req => {
+    
+    // Fetch YAML of Categories, transform to JSON
+    
+    // Loop through categories and APIs and generate markdown
+    // ### Core:
+    // - [APIs.do: Hypermedia-driven APIs](https://apis.do) ([API](https://apis.do/api), [Docs](https://apis.do/docs), [Repo](https://github.com/drivly/apis.do), [Roadmap](https://github.com/drivly/apis.do/issues/1), [Issues](https://github.com/drivly/apis.do/issues))
+    
     return new Response('') 
   }
 }
